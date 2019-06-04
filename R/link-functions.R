bad13ff8e02fe5c9bdab24cd44ce0214ef82f4a9
Joshua Rosenberg <jrosen48@github.com>
2019-06-04 10:57:47 -0400

    add function, modify_lhs_for_link(), to modify the lhs for the link function (presently only for glm models)

--- R/link-functions.R
+++ R/link-functions.R
@@ -1,18 +1,19 @@
 # link-functions.R
 
-name <- c("logit",
-          'inverse',
-          '1/mu^2', # inverse gaussian
-          'log')
+link_name <- c("logit",
+               'inverse',
+               # '1/mu^2', # inverse gaussian; removed until we're certain
+               'log')
 
 # not sure how to address this one: quasi(link = "identity", variance = "constant")
 
-greek_symbol <- c("log\\left[ \\frac { y }{ 1\\quad -\\quad y }  \\right]",
+link_formula <- c("log\\left[ \\frac { y }{ 1\\quad -\\quad y }  \\right]",
                   "\\frac { 1 }{ y }",
-                  "\\frac { 1 }{ 1/{ \\mu  }^{ 2 } } ", # inverse gaussian - correct?
+                  # "\\frac { 1 }{ 1/{ y }^{ 2 } } ", # inverse gaussian - correct?
                   "\\log ( { y )} ") # are the parentheses italicized here?
 
-link_function_frame <- data.frame(name, greek_symbol)
+link_function_df <- data.frame(link_name, link_formula,
+                               stringsAsFactors = FALSE)
 
 # how to find the link function and distribution family
 # counts <- c(18,17,15,20,10,20,25,13,12)
