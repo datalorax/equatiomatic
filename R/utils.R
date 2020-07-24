20caf32e7817a5e0d7db00f6208d0fe22a370567
Daniel Anderson <daniela@uoregon.edu>
2020-07-24 16:34:36 -0700

    added flexibility for intercept notation

--- R/utils.R
+++ R/utils.R
@@ -1,40 +1,32 @@
 #' Strict versions of \code{base::\link[base]{mapply}}
-#' 
+#'
 #' Return a vector of the corresponding type
-#' 
+#'
 #' @keywords internal
-#' 
+#'
 #' @param \dots arguments passed to \code{base::\link[base]{mapply}}
-#' 
-#' @return A vector of the corresponding type, \code{chr} = character, 
+#'
+#' @return A vector of the corresponding type, \code{chr} = character,
 #'   \code{dbl} = double, \code{lgl} = logical, and \code{int} = logical
-#' 
-
-#' @describeIn mapply_chr Return a character vector
+#'
 mapply_chr <- function(...) {
 	out <- mapply(...)
 	stopifnot(is.character(out))
 	out
 }
 
-#' @inheritParams mapply_chr
-#' @describeIn mapply_chr Return a logical vector
 mapply_lgl <- function(...) {
 	out <- mapply(...)
 	stopifnot(is.logical(out))
 	out
 }
 
-#' @inheritParams mapply_chr
-#' @describeIn mapply_chr Return an integer vector
 mapply_int <- function(...) {
 	out <- mapply(...)
 	stopifnot(is.integer(out))
 	out
 }
 
-#' @inheritParams mapply_chr
-#' @describeIn mapply_chr Return a double vector
 mapply_dbl <- function(...) {
 	out <- mapply(...)
 	stopifnot(is.double(out))
