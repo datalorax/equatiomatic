4b1f5e91f7b40e9a360eb7399e219078f43b40e8
Ellis Hughes <ehhughes@scharp.org>
2020-08-27 10:43:40 -0700

    Address issue #94: Add knit_print method. Added tests associated to it. Also moved formatting of output to format.equation so same code could be shared between print and knit_print.

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -13,3 +13,17 @@ test_that("Equation is printed correctly", {
   expect_equal(printed_tex[2], actual,
                label = "second line is the formula")
 })
+
+test_that("Equation is knit_print-ed correctly", {
+  model_simple <- lm(mpg ~ cyl + disp, data = mtcars)
+
+  knit_print_tex <- knitr::knit_print(extract_eq(model_simple))
+  actual <- "$$\n\\operatorname{mpg} = \\alpha + \\beta_{1}(\\operatorname{cyl}) + \\beta_{2}(\\operatorname{disp}) + \\epsilon\n$$"
+
+
+  expect_equal(as.character(knit_print_tex),
+               actual,
+               label = "second line is the formula")
+  expect_s3_class(knit_print_tex,
+               "knit_asis")
+})
